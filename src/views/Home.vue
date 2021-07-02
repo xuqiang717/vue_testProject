6ba09cd5f329b942fd40fe0dad91a8021761b34e
xuqiang <1335702018@qq.com>
2021-07-02 15:34:27 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to 许强的Vue应用"/>
   </div>
 </template>
 
